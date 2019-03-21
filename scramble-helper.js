d1dfd39ef0a78c4de04597247245f0ad06775580
vagrant <vagrant@vagrant.vm>
2019-03-21 22:51:20 +0000

    completed

--- scramble-helper.js
+++ scramble-helper.js
@@ -3,7 +3,7 @@
 function reverseWord(word) {
     return word.split('').reverse().join('');
 }
-console.log(reverseWord('melissa'))
+// console.log(reverseWord('melissa'))
 
 // Create a function that takes an array of words and returns a new array of the
  // words with each word reversed.
@@ -13,9 +13,10 @@ function reverseWords(words) {
     for (let word of words) {
         result.push(word.split('').reverse().join(''));
     }
-    console.log(result);
+    return result;
+
 }
-console.log(reverseWords(['melissa', 'bonnie', 'linda', 'dave']))
+// console.log(reverseWords(['melissa', 'bonnie', 'linda', 'dave']))
 
 
 // Create a function that returns a random word from an array
@@ -24,11 +25,11 @@ function randomWord(list) {
 
     return list[Math.floor(Math.random()*list.length)];
 }
-console.log(randomWord(['melissa', 'arun', 'will', 'qiaozi']))
+// console.log(randomWord(['melissa', 'arun', 'will', 'qiaozi']))
 
 // Create an array of words and save it to a variable. Using your functions create
 // a second array of reversed words.
-
+    
 
 words = ['melissa', 'arun', 'will', 'qiaozi']
 
@@ -37,23 +38,48 @@ function multiTask(words) {
     return reverseWords(words);
 }
 
-console.log(multiTask(words));
+// console.log(multiTask(words));
 
 // We could use the two arrays we've created to select a random word to show the user
 // and check their guess. How could we do that? Is there a better way?
 
+function multiTask_rand(words) {
 
+    return randomWord(multiTask(words).concat(words));
+}
+
+// console.log(multiTask_rand(words));
 // ////////////////////////////////////////////////////////////////////////////
 
 // Create a function that takes an array of words and returns a map with the keys
 // as the reversed word and the values as the original word.
 
+function createMap(words) {
 
+    const dict = new Map();
+
+    for (let word of words) {
+        dict[reverseWord(word)] = word;
+    }
+    return dict;
+}
+
+// console.log(createMap(words));
 
 // Create a function that takes two strings, `guess` and `word` and a map, like
 // the one you created above. If the first string is in the map, return the word
 // if not, console log "Sorry, incorrect. The word was ..." and include word.
 
+
+function guessingGame(guess,word,map) {
+
+    if (map[guess]) {
+        return map[guess];
+    }
+        return `Sorry, incorrect. The word was ${word}`
+    }
+
+// console.log(guessingGame('lliw',randomWord(words),createMap(words)));
 
 // ////////////////////////////////////////////////////////////////////////////
 // FURTHER STUDY
@@ -61,7 +87,20 @@ console.log(multiTask(words));
 // Create a function that scrambles a word. Use whatever method you like to
 // rearrange the letters.
 
+function shuffelWord (word){
+    let shuffledWord = '';
+    word = word.split('');
+    while (word.length > 0) {
+      shuffledWord +=  word.splice(word.length * Math.random() << 0, 1);
+    }
+    return shuffledWord;
+}
+
+console.log(shuffelWord(randomWord(words)));
+
 
 
 // Create a function that takes an array of words and returns a map with the
 // scrambled words as the keys and the original word as the values.
+
+
--- scramble-helper.py
+++ scramble-helper.py
@@ -5,7 +5,7 @@ def reverse_word(word):
 
     return word[::-1]
 
-print(reverse_word('melissa'))
+# print(reverse_word('melissa'))
 
 # Create a function that takes an array of words and returns a new array of the
  # words with each word reversed.
@@ -14,7 +14,7 @@ def reverse_words(words):
 
     return [word[::-1] for word in words]
 
-print(reverse_words(['melissa', 'bonnie', 'linda', 'dave']))
+# print(reverse_words(['melissa', 'bonnie', 'linda', 'dave']))
 
  # Create a function that returns a random word from an array
 
@@ -22,7 +22,7 @@ def random_word(list):
 
     return random.choice(list)
 
-print(random_word(['melissa', 'arun', 'will', 'qiaozi']))
+# print(random_word(['melissa', 'arun', 'will', 'qiaozi']))
 
  # Create an array of words and save it to a variable. Using your functions create
  # a second array of reversed words.
@@ -33,16 +33,16 @@ def multi_task(words):
 
     return reverse_words(words) + words
 
-print(multi_task(words))
+# print(multi_task(words))
 
  # We could use the two arrays we've created to select a random word to show the user
  # and check their guess. How could we do that? Is there a better way?
 
-def multi_task_rand(words):
+def multitask_rand(words):
 
     return random_word(reverse_words(words) + words)
 
-print(multi_task_rand(words))
+# print(multitask_rand(words))
 
  # ######################################
  # Create a function that takes an array of words and returns a map with the keys
@@ -50,18 +50,49 @@ print(multi_task_rand(words))
 
 def create_dict(words):
 
-    dict = {word[::-1]:word for word in words}
+    dict = {reverse_word(word):word for word in words}
 
     return dict
-print(create_dict(words))
+
+# print(create_dict(words))
 
 
  # Create a function that takes two strings, `guess` and `word` and a map, like
  # the one you created above. If the first string is in the map, return the word
  # if not, console log "Sorry, incorrect. The word was ..." and include word.
+
+def guessing_game(guess,word,dict):
+
+    if guess in dict.keys():
+        return dict[guess]
+    else:
+        return "Sorry, incorrect. The word was {}".format(word)
+
+
+# print(guessing_game('nura',random_word(words),create_dict(words)))
  # ######################################
  # FURTHER STUDY
  # Create a function that scrambles a word. Use whatever method you like to
  # rearrange the letters.
+from random import shuffle
+
+def word_scrable(word):
+
+    word = list(word)
+    shuffle(word)
+    return ''.join(word)
+
+# print(word_scrable('melissa'))
+    
  # Create a function that takes an array of words and returns a map with the
  # scrambled words as the keys and the original word as the values.
+
+
+def create_new_dict(words):
+
+    dict = {word_scrable(word):word for word in words}
+    return dict
+
+print(create_new_dict(words))
+
+    
